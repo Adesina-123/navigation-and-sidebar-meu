29631dcc496aed495bb091f76be5877c67796ac6
Adesina-123 <adesinaoladimeji92@gmail.com>
2020-03-12 14:47:02 -0600

    responsive landing page

---
+++ inline.js
@@ -0,0 +1,13 @@
+// npm init generate package <div class="json"></div>
+// npm init -y
+// src folder all codes javascript files
+// node  ./src/server.js run on terminal
+// npm install express --save
+
+// const express = required('express')
+// app.post('/v1/api/signin'), (req, res) =>{
+    
+// }
+// app.listen(5000, ()=> {
+//     console.log ('app is runing now')
+// })
